ee0fb9a0430ed2981654a3d82cc34a3bdc7995e5
AflaxCade <aflah.cade@gmail.com>
2024-01-09 12:09:19 +0000

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-from collections import deque
-"""method that determines if all the boxes can be opened"""
+"""BOXES BOXES"""
+
 
 def canUnlockAll(boxes):
-
-    if not boxes or len(boxes) == 1:
+    """
+    take boxes
+        create set of keys
+            go to box0
+                get all keys and add them setofkeys
+            start opening boxes from setofkeys
+                go to each box of each key
+                    and take the keys from it and add them to set of keys
+                keep loping through all setof keys
+            ignore keys that dont have box
+            track opening of boxes by a counter, if at end it
+            equal to lentgh of boxes it mean all boxes unlock
+            OPTIMIZE IDEA :
+                if we add 0 to setofkeys at start, we dont need for in 23
+    """
+    setofkeys = [0]
+    counter = 0
+    total_boxes = len(boxes)
+    index = 0
+    while index < len(setofkeys):
+        setkey = setofkeys[index]
+        for key in boxes[setkey]:
+            if key < total_boxes and key not in setofkeys and key > 0:
+                setofkeys.append(key)
+                counter += 1
+        index += 1
+    if counter == total_boxes - 1:
         return True
-    
-    keys = set(boxes[0])
-    opened = {0}
-    
-    queue = deque([0])
-    
-    while queue:
-        current_box = queue.popleft()
-        
-        for key in boxes[current_box]:
-            if key not in opened:
-                opened.add(key)
-                queue.append(key)
-                keys.update(boxes[key])
-                
-    return len(opened) == len(boxes)
+    else:
+        return False
