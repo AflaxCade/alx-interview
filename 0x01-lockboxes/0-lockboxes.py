e85218d0c37089b220639b66fb5031d4370e7a7f
AflaxCade <aflah.cade@gmail.com>
2024-01-09 12:06:52 +0000

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,21 +3,22 @@ from collections import deque
 """method that determines if all the boxes can be opened"""
 
 def canUnlockAll(boxes):
+
     if not boxes or len(boxes) == 1:
         return True
-
+    
     keys = set(boxes[0])
     opened = {0}
-
+    
     queue = deque([0])
-
+    
     while queue:
         current_box = queue.popleft()
-
+        
         for key in boxes[current_box]:
             if key not in opened:
                 opened.add(key)
                 queue.append(key)
                 keys.update(boxes[key])
-
+                
     return len(opened) == len(boxes)
