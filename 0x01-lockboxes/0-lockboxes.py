f6330ddd50b676887128f0623ec8f20dbbb86b3d
AflaxCade <aflah.cade@gmail.com>
2024-01-09 14:09:11 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 def canUnlockAll(boxes):
     keys = [0]
-    counter = 0
+    opened_boxes = 0
     total_boxes = len(boxes)
     index = 0
     while index < len(keys):
@@ -12,9 +12,9 @@ def canUnlockAll(boxes):
         for key in boxes[setkey]:
             if key < total_boxes and key not in keys and key > 0:
                 keys.append(key)
-                counter += 1
+                opened_boxes += 1
         index += 1
-    if counter == total_boxes - 1:
+    if opened_boxes == total_boxes - 1:
         return True
     else:
         return False
