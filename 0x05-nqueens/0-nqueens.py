ca550ff7031909146ebba8e5be073786dac4c369
AFLAH <aflah.cade@gmail.com>
2024-02-06 20:24:27 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,62 @@
 #!/usr/bin/python3
-"""This script solves the N-Queens problem"""
+"""
+The N queens puzzle is the challenge of placing N non-attacking queens
+on an NxN chessboard. Write a program that solves the N queens problem.
+"""
 
 import sys
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 try:
     N = int(sys.argv[1])
 except ValueError:
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if N < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
-columns = set()
-pos_diagonal = set()
-neg_diagonal = set()
 board = []
-
-# create the board positions
-for _ in range(N):
-    row = [0] * 2
+for i in range(N):
+    row = []
+    for y in range(N):
+        row.append(0)
     board.append(row)
+queens = []
 
 
-def is_safe(r, c):
-    """Check if a queen can be placed at position (r, c)"""
-    if c in columns or (r + c) in pos_diagonal or (r - c) in neg_diagonal:
-        return False
-    return True
+def available_spots(queens, board):
+    """Check and mark available spots on a chessboard."""
+    for i, row in enumerate(board):
+        for j, square in enumerate(row):
+            for x, y in queens:
+                if i == x or j == y or abs(i - x) == abs(j - y):
+                    board[i][j] = 1
 
 
-def solve_n_queens(r):
-    """recursively places N queens on an N x N chessboard"""
-    if r == N:
-        print(board)
-        return
+def queen_down(N, queens, board, final_combo):
+    """
+    Place queens on a chessboard of size N,
+    exploring all combinations and returning unique solutions.
+    """
+    if len(queens) == N:
+        sorted_queens = sorted(queens, key=lambda x: x[0])
+        if sorted_queens not in final_combo:
+            final_combo.append(queens)
+        return final_combo
+    available_spots(queens, board)
+    for i, row in enumerate(board):
+        for j, square in enumerate(row):
+            if not square:
+                board_cpy = [r[:] for r in board]
+                queen_down(N, queens + [[i, j]], board_cpy, final_combo)
+    return final_combo
 
-    for c in range(N):
-        if is_safe(r, c):
-            columns.add(c)
-            pos_diagonal.add(r + c)
-            neg_diagonal.add(r - c)
-            board[r] = [r, c]
-            solve_n_queens(r + 1)
-            # Backtrack
-            columns.remove(c)
-            pos_diagonal.remove(r + c)
-            neg_diagonal.remove(r - c)
-            board[r] = [0, 0]
 
-
-solve_n_queens(0)
+queens = queen_down(N, queens, board, [])
+for queen in queens:
+    print(queen)
