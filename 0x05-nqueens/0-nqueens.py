53fc8ac1bc6535f4d8e63866f19fa3adb907daeb
AFLAH <aflah.cade@gmail.com>
2024-02-06 20:23:27 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,83 +1,58 @@
 #!/usr/bin/python3
-"""contains the nqueens code"""
+"""This script solves the N-Queens problem"""
 
-from sys import argv, exit
+import sys
 
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-def queens(N, i, j, my_list):
-    """
-    place queens recursively
-    :param N: chessboard dimension
-    :param i: row
-    :param j: column
-    :param my_list:
-    :return:n queens
-    """
-    while i < N:
-        if validation(i, j, my_list):
-            my_list.append([i, j])
-            if j == N - 1:
-                print(my_list)
-                my_list.pop()
-            else:
-                queens(N, 0, j + 1, my_list)
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    exit(1)
 
-        i += 1
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    if len(my_list) > 0:
-        my_list.pop()
+columns = set()
+pos_diagonal = set()
+neg_diagonal = set()
+board = []
 
-    return
+# create the board positions
+for _ in range(N):
+    row = [0] * 2
+    board.append(row)
 
 
-def validation(i, j, my_list):
-    """
-    check if the position is valid
-    :param i: rows
-    :param j: columns
-    :param my_list: previously tested positions
-    :return: TRUE if valid, FALSE if not
-    """
-    rows = []
-    cols = []
-    diag1 = []
-    diag2 = []
-
-    for elem in my_list:
-        # rows and columns
-        rows.append(elem[0])
-        cols.append(elem[1])
-
-        # diagonals
-        diag1.append(elem[0] + elem[1])
-        diag2.append(elem[1] - elem[0])
-
-    if i in rows or j in cols:
+def is_safe(r, c):
+    """Check if a queen can be placed at position (r, c)"""
+    if c in columns or (r + c) in pos_diagonal or (r - c) in neg_diagonal:
         return False
-
-    if i + j in diag1 or j - i in diag2:
-        return False
-
     return True
 
 
-if __name__ == "__main__":
+def solve_n_queens(r):
+    """recursively places N queens on an N x N chessboard"""
+    if r == N:
+        print(board)
+        return
 
-    length = len(argv)
-    if length != 2:
-        print("Usage: nqueens N")
-        exit(1)
+    for c in range(N):
+        if is_safe(r, c):
+            columns.add(c)
+            pos_diagonal.add(r + c)
+            neg_diagonal.add(r - c)
+            board[r] = [r, c]
+            solve_n_queens(r + 1)
+            # Backtrack
+            columns.remove(c)
+            pos_diagonal.remove(r + c)
+            neg_diagonal.remove(r - c)
+            board[r] = [0, 0]
 
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
 
-    N = int(argv[1])
-
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    positions = []
-
-    queens(N, 0, 0, positions)
+solve_n_queens(0)
