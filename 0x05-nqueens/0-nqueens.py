b2299c5663202c7e49565cab2dabf34d27e7b042
AFLAH <aflah.cade@gmail.com>
2024-02-06 20:25:50 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,62 +1,61 @@
 #!/usr/bin/python3
-"""
-The N queens puzzle is the challenge of placing N non-attacking queens
-on an NxN chessboard. Write a program that solves the N queens problem.
-"""
+"""N Queens placement on NxN chessboard"""
+
 
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-board = []
-for i in range(N):
-    row = []
-    for y in range(N):
-        row.append(0)
-    board.append(row)
-queens = []
+def generate_solutions(row, column):
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
 
-def available_spots(queens, board):
-    """Check and mark available spots on a chessboard."""
-    for i, row in enumerate(board):
-        for j, square in enumerate(row):
-            for x, y in queens:
-                if i == x or j == y or abs(i - x) == abs(j - y):
-                    board[i][j] = 1
+def place_queen(queen, column, prev_solution):
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
 
 
-def queen_down(N, queens, board, final_combo):
-    """
-    Place queens on a chessboard of size N,
-    exploring all combinations and returning unique solutions.
-    """
-    if len(queens) == N:
-        sorted_queens = sorted(queens, key=lambda x: x[0])
-        if sorted_queens not in final_combo:
-            final_combo.append(queens)
-        return final_combo
-    available_spots(queens, board)
-    for i, row in enumerate(board):
-        for j, square in enumerate(row):
-            if not square:
-                board_cpy = [r[:] for r in board]
-                queen_down(N, queens + [[i, j]], board_cpy, final_combo)
-    return final_combo
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
 
-queens = queen_down(N, queens, board, [])
-for queen in queens:
-    print(queen)
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        n = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return (n)
+
+
+def n_queens():
+
+    n = init()
+    # generate all solutions
+    solutions = generate_solutions(n, n)
+    # print solutions
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
